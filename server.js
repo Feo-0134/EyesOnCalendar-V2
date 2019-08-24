1808d74a5f95d6dcb50b93971536cc64ee320610
Juncheng Zhu <zhujuncheng1021@163.com>
2019-08-24 15:48:01 +0800

    separate the monogodb connection from server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Database
-const mongoose = require('mongoose')
-mongoose.set('useCreateIndex', true)
+// const mongoose = require('mongoose')
+// mongoose.set('useCreateIndex', true)
 const models = require('./models/Month')
 const Month = models.Month
 const fs = require('fs')
@@ -23,20 +23,21 @@ const io = require('socket.io')(server)
 require('dotenv').config()
 
 var staticPath = ''
-var connString = ''
+// var connString = ''
 const errorMsg = 'Record not found'
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === undefined) {
   staticPath = '/dist/'
-  connString = process.env.DB_CONNECTSTRING_PRODUCTION
+  // connString = process.env.DB_CONNECTSTRING_PRODUCTION
 } else {
   // var connString = "mongodb://mayocalendarv2-dev:CiXxW30UqowaAs8CiAVyNiLgJ2UkRmpN6KXBGcJWamGmN2sNYkwcfhRhXQqGfi6jOFH6imOniww5Wn6tX2dIIA%3D%3D@mayocalendarv2-dev.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
   staticPath = './client/dist/'
-  connString = process.env.DB_CONNECTSTRING_DEV
+  // connString = process.env.DB_CONNECTSTRING_DEV
 }
 
-mongoose.connect(connString)
-const db = mongoose.connection
+// mongoose.connect(connString)
+// const db = mongoose.connection
+const db = require('./mongodb')
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
   console.log('Connected to DB')
